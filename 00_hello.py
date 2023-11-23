622d5592357803792273efc27fb7898a28a80664
darbin <celedon22cuene@gmail.com>
2023-11-23 11:24:22 -0500

    python

---
+++ 00_hello.py
@@ -0,0 +1,20 @@
+# hola comentarios de una sola linea
+
+print("hola darbin ") 
+
+"""
+esto es un comentario
+de varias lineas
+ en python
+
+
+
+#consultar el tipo de dato
+print(type(12))
+print(type(1.2))
+print(type(False))
+print(type(12+3j)) #es dato complejo
+"""
+x = 5
+print((type(x)))
+
---
+++ 01_variables.py
@@ -0,0 +1,14 @@
+variable="punetas"
+
+print("como estas" + variable)
+
+num =12
+num2=80  
+numero=num + num2
+print( "el resultado de la suma es :", numero)
+
+#para un numero random 
+
+import random
+ 
+print(random.randrange(1, 10))
